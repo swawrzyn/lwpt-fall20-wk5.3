c2bdb57f81d4fa906c2f6765bed780882407ae47
Stefan Wawrzyn <swawrzyn@gmail.com>
2020-11-14 14:05:24 +0800

    after first lecture.

--- pages/index/detail.js
+++ pages/index/detail.js
@@ -10,8 +10,19 @@ Page({
     items: [{ id: 1, text: "Loading..." }],
     scrollInto: "",
     inputVal: "",
+    inputRating: 1,
+    userInfo: null,
   },
   onLoad: function (options) {
+    // get currentUser information
+    const userInfo = wx.getStorageSync("userInfo");
+
+    if (userInfo) {
+      this.setData({
+        userInfo: userInfo,
+      });
+    }
+
     const Movies = new wx.BaaS.TableObject("movies");
     const MovieReviews = new wx.BaaS.TableObject("movie_reviews");
 
@@ -50,19 +61,58 @@ Page({
     const val = this.data.inputVal;
 
     if (val.trim() === "") return;
-    const items = this.data.items;
-    const nextId = items.length + 1;
-    items.push({ id: nextId, name: val });
-    this.setData({
-      items,
+
+    // get the movie_reviews table
+    const Reviews = new wx.BaaS.TableObject("movie_reviews");
+
+    // step 1: create a blank record
+    const newReview = Reviews.create();
+
+    // step 2: set the information in the record
+    newReview.set({
+      text: val,
+      rating: this.data.inputRating,
+      movieId: this.data.movie.id,
     });
-    setTimeout(() => {
-      this.setData({
-        scrollInto: `item-${nextId}`,
-      });
-    }, 250);
+
+    // send the record to ifanr to save in the database
+    newReview.save().then(
+      (res) => {
+        console.log("newReview save", res);
+        const newItems = this.data.items;
+        newItems.push(res.data);
+        this.setData({
+          items: newItems,
+        });
+      },
+      (error) => {
+        console.log("newReview save error, error");
+      }
+    );
+
     this.setData({
       inputVal: "",
+    });
+  },
+  userInfoHandler: function (data) {
+    wx.BaaS.auth.loginWithWechat(data).then(
+      (res) => {
+        // user login successful, user authorized
+        this.setData({
+          userInfo: res,
+        });
+
+        // save the userInfo inside of our phone storage
+        wx.setStorageSync("userInfo", res);
+      },
+      (err) => {
+        console.log("login error", err);
+      }
+    );
+  },
+  onRatingChange: function (e) {
+    this.setData({
+      inputRating: Number.parseInt(e.detail.value),
     });
   },
 });
